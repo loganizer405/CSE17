79e4baa869d21b79e065e0dee194e0f6604463fb
Logan Kramer <loganizer405@gmail.com>
2020-01-21 16:22:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CSE17
+Programs for CSE017 at Lehigh University (Private)
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Author: Logan Kramer
+ * Program Name:
+ * Date:
+ * Version: 1.0
+ */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
