d9c444951756ff495bc59dfdae8edf935212bada
Logan Kramer <loganizer405@gmail.com>
2020-01-21 16:34:31 -0500

    This is a test for git from IntelliJ.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 /*
  * Author: Logan Kramer
  * Program Name:
- * Date:
+ * Date: 1/21/20
  * Version: 1.0
  */
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("test ");
     }
 }
